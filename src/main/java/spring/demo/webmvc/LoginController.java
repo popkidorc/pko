e6e1eb14f54e482699a880691b52fe7801dbe497
popkidorc <pop.kid.orc@163.com>
2015-03-24 18:48:20 +0800

    1.spring mvc annotation

--- src/main/java/spring/demo/webmvc/LoginController.java
+++ src/main/java/spring/demo/webmvc/LoginController.java
@@ -5,22 +5,23 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
-//@Controller
-public class LoginController implements Controller {
+@Controller
+public class LoginController {
 	static Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-	// // @RequestMapping(value = "login")
-	// public ModelAndView login(HttpServletRequest request,
-	// HttpServletResponse response, LoginForm command) {
-	// String username = command.getUsername();
-	// logger.error("message " + username);
-	// ModelAndView mv = new ModelAndView("/login/form");
-	// mv.addObject("command_user", "LOGIN SUCCESS, " + username);
-	// return mv;
-	// }
+	@RequestMapping(value = "login")
+	public ModelAndView login(HttpServletRequest request,
+			HttpServletResponse response, LoginForm command) {
+		String username = command.getUsername();
+		logger.error("message " + username);
+		ModelAndView mv = new ModelAndView("/login/form");
+		mv.addObject("command_user", "LOGIN SUCCESS, " + username);
+		return mv;
+	}
 
 	// @Override
 	// public ModelAndView handle(HttpServletRequest request,
@@ -32,12 +33,13 @@ public class LoginController implements Controller {
 	// return mv;
 	// }
 
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request,
-			HttpServletResponse response) throws Exception {
-		ModelAndView mv = new ModelAndView("/login/form");
-		mv.addObject("command_user", "LOGIN SUCCESS, ");
-		return mv;
-	}
+	// @Override
+	// public ModelAndView handleRequest(HttpServletRequest request,
+	// HttpServletResponse response) throws Exception {
+	// String name = request.getParameter("username");
+	// ModelAndView mv = new ModelAndView("/login/form");
+	// mv.addObject("command_user", "LOGIN SUCCESS, " + name);
+	// return mv;
+	// }
 
 }
